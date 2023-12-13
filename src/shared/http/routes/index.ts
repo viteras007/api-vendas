71ae65f3a5f38168730350b5e6e50f447fa08723
viteras007 <viteras10@gmail.com>
2023-12-13 14:00:44 -0300

    [feat] - first commit

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,13 @@
+import productsRouter from '@modules/products/routes/products.routes';
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.use('/products', productsRouter);
+
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Hello Dev' });
+
+})
+
+export default routes;
