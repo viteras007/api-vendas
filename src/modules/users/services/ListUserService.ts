76c23e8b0fbfe427d9e47bab9d1a974b59a3d25a
viteras007 <viteras10@gmail.com>
2023-12-13 15:18:18 -0300

    feat: CRUD users

---
+++ src/modules/users/services/ListUserService.ts
@@ -0,0 +1,14 @@
+import { getCustomRepository } from "typeorm";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+import User from "../typeorm/entities/User";
+
+class ListUserService {
+  public async execute (): Promise<User[]> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const users = await usersRepository.find();
+
+    return users;
+  }
+}
+
+export default ListUserService;
